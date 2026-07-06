4dbdd41e9b0e7f61e0890d6e4727deef2ce3a820
Derek Lewis <7467911+derekelewis@users.noreply.github.com>
2026-07-06 18:56:56 +0000

    add TODO

--- src/response.cc
+++ src/response.cc
@@ -7,6 +7,7 @@ std::string Response::serialize() const
     std::ostringstream response;
 
     response << "HTTP/1.1 " << status_code_ << " " << http::status_code_phrase(status_code_) << "\r\n";
+    // TODO: implement returning a real time
     response << "Date: " << "Thu, 02 Jul 2026 19:13:26 GMT" << "\r\n";
     response << "Content-Type: text/html" << "\r\n";
     response << "Content-Length: " << body_.size() << "\r\n";
